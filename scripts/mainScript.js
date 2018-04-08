40009356c483794874a9ef8268e1d76b6ff6bf54
theo v <theo.viardin1@etu.univ-nantes.fr>
2018-04-08 15:06:51 +0200

    fdrv

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -43,13 +43,13 @@ $(window).on('load', function () {
   });
 
   $("#faireRequete").on("click", function() {
-    $.get("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dc96eac4784f4c3f7a051dd0363ca4a3&text=["+$("#inputNomDeVille").val()+"]&format=json&nojsoncallback=?&per_page="+$("#nombreDePhoto").val(), function (response) {
+    $.get("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=56097ac8c6cf97b680539ef25f536f32&text=["+$("#inputNomDeVille").val()+"]&format=json&nojsoncallback=?&per_page="+$("#nombreDePhoto").val(), function (response) {
       if ($("#date").datepicker("getDate") == null) {
         $("#tableOnglet-2").empty().append("<thead><tr><th>Image</th><th>Titre</th><th>Description</th><th>Propriétaire</th><th>Date</th></tr></thead><tbody></tbody>")
         $("#onglet-1").empty();
         if (response.photos.photo.length > 0) {
           for (photo of response.photos.photo) {
-            $.get("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=dc96eac4784f4c3f7a051dd0363ca4a3&photo_id="+photo.id+"&format=json&nojsoncallback=?", function (response2) {
+            $.get("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=56097ac8c6cf97b680539ef25f536f32&photo_id="+photo.id+"&format=json&nojsoncallback=?", function (response2) {
               $("#onglet-1").append("<img class='ongl1' data-id="+response2.photo.id+"   src='https://farm"+response2.photo.farm+".staticflickr.com/"+response2.photo.server+"/"+response2.photo.id+"_"+response2.photo.secret+".jpg' data-desc='"+response2.photo.description._content+"' data-owner='"+response2.photo.owner.username+"' data-heure='"+response2.photo.dates.taken+"' data-name='"+response2.photo.title._content+"'>")
 
               $("#tableOnglet-2 tbody").append("<tr><td><img class='ongl2' style='width:100px; height:auto;' src='https://farm"+response2.photo.farm+".staticflickr.com/"+response2.photo.server+"/"+response2.photo.id+"_"+response2.photo.secret+".jpg'></td><td>"+response2.photo.title._content+"</td><td>"+response2.photo.description._content+"</td><td>"+response2.photo.owner.username+"</td><td>"+response2.photo.dates.taken+"</td></tr>")
@@ -91,7 +91,7 @@ $(window).on('load', function () {
         $("#onglet-1").empty();
         if (response.photos.photo.length > 0) {
           for (photo of response.photos.photo) {
-            $.get("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=dc96eac4784f4c3f7a051dd0363ca4a3&photo_id="+photo.id+"&format=json&nojsoncallback=?", function (response2) {
+            $.get("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=56097ac8c6cf97b680539ef25f536f32&photo_id="+photo.id+"&format=json&nojsoncallback=?", function (response2) {
               if (new Date(response2.photo.dates.taken).getTime() > date.getTime()) {
                 $("#onglet-1").append("<img class='ongl1' data-id="+response2.photo.id+"   src='https://farm"+response2.photo.farm+".staticflickr.com/"+response2.photo.server+"/"+response2.photo.id+"_"+response2.photo.secret+".jpg' data-desc='"+response2.photo.description._content+"' data-owner='"+response2.photo.owner.username+"' data-heure='"+response2.photo.dates.taken+"' data-name='"+response2.photo.title._content+"'>")
 
